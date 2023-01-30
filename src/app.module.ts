a00e89256dca2d844244c24f0f206d379654b3f8
Thiago Martins <thiago_valentim1@hotmail.com>
2023-01-30 10:59:42 -0300

    feat: add hashing service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [CoffeesModule, UsersModule, DatabaseModule],
+  imports: [CoffeesModule, UsersModule, DatabaseModule, IamModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/iam/hashing/bcrypt.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { HashingService } from './hashing.service';
+import { hash, genSalt, compare as compareStrings } from 'bcrypt';
+
+@Injectable()
+export class BcryptService implements HashingService {
+  async hash(data: string | Buffer): Promise<string> {
+    const salt = await genSalt();
+    return hash(data, salt);
+  }
+  compare(data: string | Buffer, encrypted: string): Promise<boolean> {
+    return compareStrings(data, encrypted);
+  }
+}
