fba98e9ef5801fa074f194f855c8e4271eb47728
Thiago Martins <thiago_valentim1@hotmail.com>
2023-01-30 11:07:16 -0300

    feat: create sign in method

---
+++ src/iam/authentication/authentication.service.ts
@@ -0,0 +1,28 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { HashingService } from '../hashing/hashing.service';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  public async signUp(signUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (error.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException('Email already exists');
+      }
+      throw error;
+    }
+  }
+}
--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
 
 @Module({
   providers: [
@@ -8,6 +10,8 @@ import { BcryptService } from './hashing/bcrypt.service';
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthenticationService,
   ],
+  controllers: [AuthenticationController],
 })
 export class IamModule {}
